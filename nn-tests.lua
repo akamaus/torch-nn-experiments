64f26ffcab86516000b96194ac6d0f7055c6cd2d
Dmitry Vyal <akamaus@gmail.com>
2016-02-21 22:35:34 +0300

    modularized

--- nn-tests.lua
+++ nn-tests.lua
@@ -2,37 +2,44 @@ torch = require 'torch'
 nn = require 'nn'
 gp = require 'gnuplot'
 
-l1 = nn.Linear(2,1)
-net = nn.Sequential()
-cr = nn.MSECriterion()
+function build_net1()
+   local l1 = nn.Linear(2,1)
+   local net = nn.Sequential()
+   net:add(l1)
+   return net
+end
 
-net:add(l1)
-
-function learn_step(model)
+function learn_step(model, func, cr)
   -- random sample
   local input= torch.rand(2);     -- normally distributed example in 2d
   local output= torch.Tensor(1);
 
-  output[1] = 2 * input[1] + 3 * input[2] - 5
+  output[1] = func(input[1], input[2])
 
   -- feed it to the neural network and the criterion
-  local o = net:forward(input)
-  cr:forward(o, output)
+  local o = model:forward(input)
+--  cr:forward(o, output)
 
   -- train over this example in 3 steps
   -- (1) zero the accumulation of the gradients
   model:zeroGradParameters()
   -- (2) accumulate gradients
-  model:backward(input, cr:backward(net.output, output))
+  model:backward(input, cr:backward(model.output, output))
   -- (3) update parameters with a 0.01 learning rate
   model:updateParameters(0.01)
   return (o[1] - output[1])*(o[1] - output[1])
 end
 
-errs = torch.Tensor(10000)
-for i=1,1000 do
-   local e = learn_step(net)
-   errs[i] = e
-   print(e)
+function disp_train(net, func, len)
+   local cr = nn.MSECriterion()
+   local errs = torch.Tensor(len)
+
+   for i=1,len do
+      local e = learn_step(net, func, cr)
+      errs[i] = e
+   end
+   gp.plot("errs", errs, '+')
 end
-gp.plot("errs", errs, '+')
+
+n1 = build_net1()
+disp_train(n1, function(x,y) return 3* x - 10*y + 3 end, 2000)
