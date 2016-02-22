5fd6ec306f2df1007e4d32c399fd5098b3c66fc0
Dmitry Vyal <akamaus@gmail.com>
2016-02-22 11:10:10 +0300

    two-layer network, multiplots

--- nn-tests.lua
+++ nn-tests.lua
@@ -6,13 +6,15 @@ function build_net(w)
    local net = nn.Sequential()
    net:add(nn.Linear(1,w))
    net:add(nn.Tanh())
+   net:add(nn.Linear(w,w))
+   net:add(nn.Tanh())
    net:add(nn.Linear(w,1))
    return net
 end
 
 function learn_step(model, func, cr)
   -- random sample
-  local input= torch.randn(1) * 10;     -- normally distributed example in 2d
+  local input= torch.rand(1) * 10 - 5;     -- normally distributed example in 2d
   local output= torch.Tensor(1);
 
   output[1] = func(input[1])
@@ -39,17 +41,33 @@ function disp_train(net, func, len)
       local e = learn_step(net, func, cr)
       errs[i] = e
    end
---   gp.plot("errs", errs, '+')
+
+   local ch = 1000
+   local ch_e = errs:chunk(ch)
+
+   local es = map(function(cs) return cs:sum() / cs:size(1) end, ch_e)
+
+   gp.plot("errs", torch.Tensor(es), '-')
 end
 
 function f(x)
    return -math.sin(3*x) + math.cos(2*x + 1)
 end
 
+function map(func, tbl)
+     local newtbl = {}
+     for i,v in pairs(tbl) do
+         newtbl[i] = func(v)
+     end
+     return newtbl
+end
+
 for w=1,10 do
    local nt = build_net(w)
 
-   disp_train(nt, f, 20000)
+   gp.figure(w)
+   gp.raw("set multiplot layout 2,1")
+   disp_train(nt, f, 50000)
 
    xx = torch.linspace(-5,5,100)
    yy1 = xx:clone()
@@ -58,6 +76,6 @@ for w=1,10 do
    yy1 = yy1:apply(function(x) return nt:forward(torch.Tensor({x}))[1] end)
    yy2 = yy2:apply(f)
 
-   gp.figure(w)
    gp.plot({"func", xx,yy1}, {"tgt", xx, yy2})
+   gp.raw("unset multiplot")
 end
